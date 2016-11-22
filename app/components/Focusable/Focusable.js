e1a856399eb957a663c1550418880d76da218076
Vladimir Afanasyev <vajs@yandex-team.ru>
2016-11-22 17:48:19 +0300

    Some work

--- app/components/Focusable/Focusable.js
+++ app/components/Focusable/Focusable.js
@@ -7,7 +7,31 @@ let focusComponent = null;
 
 const selfPropKeys = new Set(['onFocus', 'onBlur', 'onClick']);
 
-
+/**
+ * Компонент, на который можно поставить фокус, не использует браузерный фокус и tabindex
+ * Почему просто не использовать браузерный фокус? С браузерным фокусом, если внутри одного
+ * фокусируемого элемента находится другой фокусируемый элемент, то при переходе фокуса из
+ * родительского к дочернему на родительском триггерится blur, а это не нужно во всех кейсах,
+ * с которыми я сталкивался
+ * В данном случае фокус работает так
+ * Допустим, у нас есть следующая иерархия фокусируемых элементов, вложенных друг вдруга:
+ *        A
+ *        |
+ *        B
+ *      /   \
+ *     C     D
+ *          /  \
+ *         E    F
+ * Сначала в фокусе находится B
+ * Дальше переносим фокус на F, в результате на элементах D и F сработает событие onFocus
+ * Переносим фокус на E - на F сработает onBlur, на E - onFocus
+ * Переносим фокус на C - на E и D сработает onBlur, на C - onFocus
+ * Переносим фокус на B - на C сработает onBlur
+ *
+ * Фокус ставится при клике на элемент, причем в тот момент, когда событие всплывет до корневого
+ * фокусируемого элемента. Если где-то по пути в событии onClick у объекта события вызвали preventDefault,
+ * то фокус не поставится
+ */
 export default class Focusable extends Component {
     constructor(props) {
         super(props);
@@ -28,16 +52,14 @@ export default class Focusable extends Component {
             onClick(event);
         }
 
-        if (event.defaultPrevented) {
-            return;
-        }
-
         const {parentFocusable} = this.context;
 
         focusComponent = focusComponent || this;
 
         if (!parentFocusable) {
-            focusComponent.focus();
+            if (!event.defaultPrevented) {
+                focusComponent.focus();
+            }
             focusComponent = null;
         }
     }
@@ -46,11 +68,18 @@ export default class Focusable extends Component {
         return this.context.parentFocusable || null;
     }
 
+    /**
+     * Ставит фокус на элемент
+     */
     focus() {
 
         focusToNode(this);
     }
 
+    /**
+     * Если элемент находится в фокусе - ставит фокус на родительский фокусируемый элемент,
+     * иначе ничего не делает
+     */
     blur() {
         if (!this._isFocused) {
             return;
@@ -76,6 +105,9 @@ export default class Focusable extends Component {
         />
     }
 
+    /**
+     * Находится ли элемент в фокусе
+     */
     get isFocused() {
         return this._isFocused;
     }
